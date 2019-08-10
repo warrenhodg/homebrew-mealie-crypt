934137c264d4b8c246982a6817548747f1bf5ba2
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2019-08-10 09:49:16 +0100

    Added mealie-crypt.rb

---
+++ mealie-crypt.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class MealieCrypt < Formula
+  desc "A tool for groups of people to encrypt key-value pairs using their ssh keys"
+  homepage "https://github.com/warrenhodg/mealie-crypt"
+  url "https://github.com/warrenhodg/mealie-crypt/releases/download/1.2.3/mealie-crypt.bottle.tgz"
+  sha256 "642a0d3cf0a3f365c38621b903c0407c659ff63c3496c0a85979c7c191afaf73"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test mealie-crypt`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
