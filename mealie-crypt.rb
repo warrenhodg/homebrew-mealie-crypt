1a6e5f59c163556252c9e3885172aeef4d063f6f
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2019-08-10 10:13:53 +0100

    Updated mealie-crypt.rb

--- mealie-crypt.rb
+++ mealie-crypt.rb
@@ -1,35 +1,17 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class MealieCrypt < Formula
-  desc "A tool for groups of people to encrypt key-value pairs using their ssh keys"
+  desc "A tool for groups of people to manage enrypted key-value pairs using ssh keys"
   homepage "https://github.com/warrenhodg/mealie-crypt"
-  url "https://github.com/warrenhodg/mealie-crypt/releases/download/1.2.3/mealie-crypt.bottle.tgz"
-  sha256 "642a0d3cf0a3f365c38621b903c0407c659ff63c3496c0a85979c7c191afaf73"
+  url "https://github.com/warrenhodg/mealie-crypt/archive/1.2.3.tar.gz"
+  sha256 "9ebd49b74dae89b5259d2b4cc5dfdd2277e6aa5a914c169cafbc5a4db35e3708"
 
-  # depends_on "cmake" => :build
+  depends_on "go" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "mac"
+    bin.install "./mealie-crypt.mac" => "mealie-crypt"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test mealie-crypt`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/mealie-crypt" "--version"
   end
 end
